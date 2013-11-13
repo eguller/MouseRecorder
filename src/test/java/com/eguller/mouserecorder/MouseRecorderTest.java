622507f02d0290b30d5203849cebfaa19d23c08b
eguller <enginguller@gmail.com>
2013-11-13 18:21:19 +0100

    remove intellij project

---
+++ src/test/java/com/eguller/mouserecorder/MouseRecorderTest.java
@@ -0,0 +1,38 @@
+package MouseRecorder;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class MouseRecorderTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public MouseRecorderTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( MouseRecorderTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
