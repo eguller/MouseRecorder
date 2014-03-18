71d7555d465a8b6512e4a751f90315af1dfa1a72
eguller <enginguller@gmail.com>
2014-03-18 09:15:03 +0100

    get version

--- src/main/java/com/eguller/mouserecorder/ui/action/AboutAction.java
+++ src/main/java/com/eguller/mouserecorder/ui/action/AboutAction.java
@@ -47,7 +47,7 @@ public class AboutAction implements ActionListener {
         Class clazz = AboutAction.class;
         String className = clazz.getSimpleName() + ".class";
         String classPath = clazz.getResource(className).toString();
-        if (!classPath.startsWith("jar") || !classPath.startsWith("exe")) {
+        if (!classPath.startsWith("jar")) {
             // Class not from JAR
             return version;
         }
